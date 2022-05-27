7422f69bf60aeb1c7d15d6744b064b38f7142f04
Andrew Tokeley <andrewtokeley@gmail.com>
2022-05-28 08:26:53 +1200

    initial commit

---
+++ numbermarch/Extensions/String.swift
@@ -0,0 +1,20 @@
+//
+//  String.swift
+//  numbermarch
+//
+//  Created by Andrew Tokeley on 16/05/22.
+//
+
+import Foundation
+
+extension String {
+    func leftPadding(toLength: Int, withPad character: Character) -> String {
+        let stringLength = self.count
+        if stringLength < toLength {
+            return String(repeatElement(character, count: toLength - stringLength)) + self
+        } else {
+            return String(self.suffix(toLength))
+        }
+    }
+}
+  
